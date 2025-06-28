947b91eb99e141b4c29cb926be53f9b9563e67b7
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-06-28 01:41:57 +0100

    fixed the allowed hosts bug

--- election_portal/settings.py
+++ election_portal/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1','gttech.pythonanywhere.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,gttech.pythonanywhere.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
