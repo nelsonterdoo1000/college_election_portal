77776b85fed95704db19f1810f17580dc2e8ea11
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-08-04 17:06:21 +0100

    added students for final upload

--- election_portal/settings.py
+++ election_portal/settings.py
@@ -12,7 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+# DEBUG = os.getenv('DEBUG', 'True') == 'True'
+
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,213.199.34.226,cloudinary.com,api.nocenelections.com,nocenelections.com').split(',')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,213.199.34.226,cloudinary.com,api.nocenelections.com,nocenelections.com',).split(',')
